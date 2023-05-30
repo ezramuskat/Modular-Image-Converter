37c659a77b3c0648e2109a2d8adb23946bbff009
Ezra Muskat <ezramuskat@gmail.com>
2023-05-29 23:59:40 -0400

    added skeleton for png struct and ConvertibleImage trait

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::{io, path::Path};
+
+mod png;
 fn main() {
     println!("Hello, world!");
+}
+
+trait ConvertibleImage {
+    fn from_file<P: AsRef<Path>>(path: P) -> io::Result<Box<Self>>;
+    fn to_png(&self) -> png::Png;
+    fn from_png(png: png::Png) -> Self;
 }
